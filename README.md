b1669c5b26ed63cc83a2cff8c4c2f837540c9039
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 17:57:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es el ejercicio 18 correspondiente al capitulo 3
---
+++ src/main/java/john/ejercicio18cap3poo/Ejercicio18Cap3Poo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio18cap3poo;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio18Cap3Poo {
+
+    public static void main(String[] args) {
+	    IngresoDeDatos form = new IngresoDeDatos();
+	    form.setVisible(true);
+    }
+}
